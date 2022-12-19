26ea6161ebdc1eb9ac9b49d15358595669e3fde8
Benjamin Frazer <benjamin.frazer.1997@gmail.com>
2022-12-19 13:07:08 +0000

    fixed init.py

--- buckfs/__init__.py
+++ buckfs/__init__.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 from .remote import Remote
 from .bfs import BFS
+from .syncJob import SyncJob
+from .bucket import Bucket
+from .objectCollection import ObjectCollection
+from .object import Object
+from .localObject import LocalObject
